4bd03a1ff0d8b0afefb1a535200cf9dcce3f6652
MadhavGupta <gmadhav857@gmail.com>
2026-04-01 13:52:43 +0530

    added an add route with int type casted

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Hello, Wor d!</h1>"
+@app.route('/greet/<name>')
+def greet(name  ):
+    return f"<h1>Hello {name}!</h1>"
+
+@app.route("/add/<int:a>/<int:b>")
+def add(a, b):
+    return f"<h1>{a} + {b} = {a + b}</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
