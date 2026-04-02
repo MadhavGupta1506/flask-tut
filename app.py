4286fcb87cb2855ff8fca9ce5f6057f7146287e6
MadhavGupta <gmadhav857@gmail.com>
2026-04-02 12:33:55 +0530

    added custom template_filter

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
-    return "<h1>Hello, Wor d!</h1>"
+    myval="Hello, Flask!"
+    myres=54
+    mylist=[1,2,3,4,5]
+    return render_template('index.html', myval=myval, myres=myres, mylist=mylist)
+
+@app.route('/myhtml')
+def show_myhtml():
+    return render_template('myhtml.html',some_text="This is some text passed to the template")
+
+@app.template_filter('reverse')
+def reverse_filter(s):
+    return s[::-1]
+
 @app.route('/greet/<name>')
 def greet(name  ):
     return f"<h1>Hello {name}!</h1>"
